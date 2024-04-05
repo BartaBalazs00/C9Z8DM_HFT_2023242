20814dc12093c6f513d46575bd6476700b92ef72
Balázs <Balzs@DESKTOP-PQQJN26>
2024-04-05 11:27:02 +0200

    Add project files.

---
+++ C9Z8DM_HFT_2023242.Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace C9Z8DM_HFT_2023242.Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
