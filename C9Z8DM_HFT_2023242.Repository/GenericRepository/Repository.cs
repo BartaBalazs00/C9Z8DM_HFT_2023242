c9dceca20428bc214234fbc7d3b1c26177d2c1a2
Barta Balázs <barta.balazs00@gmail.com>
2024-04-24 14:32:00 +0200

    Repository created

---
+++ C9Z8DM_HFT_2023242.Repository/GenericRepository/Repository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C9Z8DM_HFT_2023242.Repository
+{
+    public abstract class Repository<T> : IRepository<T> where T : class
+    {
+        protected SchoolDbContext ctx;
+        public Repository(SchoolDbContext ctx)
+        {
+            this.ctx = ctx;
+        }
+        public void Create(T item)
+        {
+            ctx.Set<T>().Add(item);
+            ctx.SaveChanges();
+        }
+
+        public IQueryable<T> ReadAll()
+        {
+            return ctx.Set<T>();
+        }
+
+        public void Delete(int id)
+        {
+            ctx.Set<T>().Remove(Read(id));
+            ctx.SaveChanges();
+        }
+
+        public abstract T Read(int id);
+        public abstract void Update(T item);
+
+    }
+}
