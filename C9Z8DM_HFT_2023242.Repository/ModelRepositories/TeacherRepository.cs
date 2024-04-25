96e73439096e78bea3232b35fb6fedb90dcfae4f
BartaBalazs00 <barta.balazs00@gmail.com>
2024-04-25 09:51:51 +0200

    TeacherRepository created

---
+++ C9Z8DM_HFT_2023242.Repository/ModelRepositories/TeacherRepository.cs
@@ -0,0 +1,31 @@
+﻿using C9Z8DM_HFT_2023242.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C9Z8DM_HFT_2023242.Repository
+{
+    internal class TeacherRepository : Repository<Teacher>, IRepository<Teacher>
+    {
+        public TeacherRepository(SchoolDbContext ctx) : base(ctx)
+        {
+        }
+
+        public override Teacher Read(int id)
+        {
+            return ctx.Teachers.FirstOrDefault(t => t.TeacherId == id);
+        }
+
+        public override void Update(Teacher item)
+        {
+            var old = Read(item.TeacherId);
+            foreach (var prop in old.GetType().GetProperties())
+            {
+                prop.SetValue(old, prop.GetValue(item));
+            }
+            ctx.SaveChanges();
+        }
+    }
+}
