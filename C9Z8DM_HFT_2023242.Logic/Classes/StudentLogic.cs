075df9eb0e382bfc24b845d3c7cc8c0ff9e04d54
BartaBalazs00 <barta.balazs00@gmail.com>
2024-04-29 13:39:29 +0200

    StudentLogic created

---
+++ C9Z8DM_HFT_2023242.Logic/Classes/StudentLogic.cs
@@ -0,0 +1,58 @@
+﻿using C9Z8DM_HFT_2023242.Models;
+using C9Z8DM_HFT_2023242.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C9Z8DM_HFT_2023242.Logic
+{
+    public class StudentLogic : IStudentLogic
+    {
+        IRepository<Student> repo;
+
+        public StudentLogic(IRepository<Student> repo)
+        {
+            this.repo = repo;
+        }
+
+        public void Create(Student item)
+        {
+            if (item.StudentName == null || item.StudentClass == null)
+            {
+                throw new ArgumentException("All properties must be given");
+            }
+            if (item.StudentName.Length < 5)
+            {
+                throw new ArgumentException("StudentName is too short");
+            }
+            this.repo.Create(item);
+        }
+
+        public void Delete(int id)
+        {
+            this.repo.Delete(id);
+        }
+
+        public Student Read(int id)
+        {
+            var student = this.repo.Read(id);
+            if (student == null)
+            {
+                throw new ArgumentException("Student not exists");
+            }
+            return this.repo.Read(id);
+        }
+
+        public IQueryable<Student> ReadAll()
+        {
+            return this.repo.ReadAll();
+        }
+
+        public void Update(Student item)
+        {
+            this.repo.Update(item);
+        }
+    }
+}
