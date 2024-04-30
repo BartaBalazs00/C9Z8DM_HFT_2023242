b6cf870d3f5ee0a3dac2fae666b5c526ad5f65ef
BartaBalazs00 <barta.balazs00@gmail.com>
2024-04-30 08:32:19 +0200

    Test Class1.cs renamed to TestClass.cs

--- C9Z8DM_HFT_2023242.Test/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace C9Z8DM_HFT_2023242.Test
-{
-    public class Class1
-    {
-    }
-}
---
+++ C9Z8DM_HFT_2023242.Test/TestClass.cs
@@ -0,0 +1,10 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace C9Z8DM_HFT_2023242.Test
+{
+    [TestFixture]
+    public class TestClass
+    {
+    }
+}
