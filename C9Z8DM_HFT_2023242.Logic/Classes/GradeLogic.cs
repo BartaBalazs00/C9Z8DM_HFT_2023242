d8496331d78cae0b2cb5062a2f3f153f24e9e63d
BartaBalazs00 <barta.balazs00@gmail.com>
2024-05-01 17:07:15 +0200

    GradeLogic 5 non CRUD methods created

--- C9Z8DM_HFT_2023242.Logic/Classes/GradeLogic.cs
+++ C9Z8DM_HFT_2023242.Logic/Classes/GradeLogic.cs
@@ -1,6 +1,8 @@
 ﻿using C9Z8DM_HFT_2023242.Models;
 using C9Z8DM_HFT_2023242.Repository;
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace C9Z8DM_HFT_2023242.Logic
@@ -54,6 +56,95 @@ namespace C9Z8DM_HFT_2023242.Logic
         public void Update(Grade item)
         {
             this.repo.Update(item);
+        }
+
+
+        //non cruds
+        public double? GetAvarageGradesPerYear(int year)
+        {
+            return this.repo
+                .ReadAll()
+                .Where(x => x.Year == year)
+                .Average(x=> x.GradeValue);
+        }
+        public double? GetAvarageGradesPerSubject(string subject)
+        {
+            return this.repo
+                .ReadAll()
+                .Where (x => x.Subject == subject)
+                .Average(x => x.GradeValue);
+        }
+        public IEnumerable<GradeInfo> GetAvarageGradesPerStudents() 
+        {
+            return from x in this.repo.ReadAll()
+                   group x by x.StudentId into g
+                   select new GradeInfo()
+                   {
+                       Id = g.Key,
+                       AvgGradeValue = g.Average(x => x.GradeValue)
+                   };
+        }
+        public IEnumerable<GradeInfoPerSubject> GetAvarageGradesPerSubjects()
+        {
+            return from x in this.repo.ReadAll()
+                   group x by x.Subject into g
+                   select new GradeInfoPerSubject()
+                   {
+                       Subject = g.Key,
+                       AvgGradeValue = g.Average(x => x.GradeValue)
+                   };
+        }
+        public int GetNumbersOfStudentsThatHasFailed()
+        {
+            return this.repo
+                .ReadAll()
+                .Where(x => x.GradeValue == 1)
+                .Count();
+        }
+    }
+    public class GradeInfoPerSubject
+    {
+        public string Subject { get; set; }
+        public double? AvgGradeValue{ get; set; }
+        public override bool Equals(object obj)
+        {
+            GradeInfoPerSubject gradeInfoPerSubject = obj as GradeInfoPerSubject;
+            if (gradeInfoPerSubject == null)
+            {
+                return false;
+            }
+            else
+            {
+                return this.Subject == gradeInfoPerSubject.Subject
+                    && this.AvgGradeValue == gradeInfoPerSubject.AvgGradeValue;
+            }
+        }
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(this.Subject, this.AvgGradeValue);
+        }
+    }
+    public class GradeInfo
+    {
+        public int Id { get; set; }
+        public double? AvgGradeValue { get; set; }
+        public override bool Equals(object obj)
+        {
+            GradeInfo gradeInfo = obj as GradeInfo;
+            if (gradeInfo == null)
+            {
+                return false;
+            }
+            else
+            {
+                return this.Id == gradeInfo.Id
+                    && this.AvgGradeValue == gradeInfo.AvgGradeValue;
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(this.Id, this.AvgGradeValue);
         }
     }
 }
