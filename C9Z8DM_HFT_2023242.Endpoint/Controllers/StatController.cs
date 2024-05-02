a44c557b5d3ecb8b59a1a91b7bc9bb326f9a3bc8
BartaBalazs00 <barta.balazs00@gmail.com>
2024-05-02 21:29:32 +0200

    StatController created

---
+++ C9Z8DM_HFT_2023242.Endpoint/Controllers/StatController.cs
@@ -0,0 +1,53 @@
+﻿using C9Z8DM_HFT_2023242.Logic;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace C9Z8DM_HFT_2023242.Endpoint.Controllers
+{
+    [Route("[controller]/[action]")]
+    [ApiController]
+    public class StatController : ControllerBase
+    {
+        IGradeLogic gradeLogic;
+        IStudentLogic studentLogic;
+
+        public StatController(IGradeLogic gradeLogic, IStudentLogic studentLogic)
+        {
+            this.gradeLogic = gradeLogic;
+            this.studentLogic = studentLogic;
+        }
+
+        [HttpGet("{year}")]
+        public double? AvarageGradesPerYear(int year)
+        {
+            return this.gradeLogic.GetAvarageGradesPerYear(year);
+        }
+        [HttpGet("{subject}")]
+        public double? AvarageGradesPerSubject(string subject)
+        {
+            return this.gradeLogic.GetAvarageGradesPerSubject(subject);
+        }
+        [HttpGet]
+        public IEnumerable<GradeInfo> AvarageGradesPerStudents()
+        { 
+            return this.gradeLogic.GetAvarageGradesPerStudents();
+        }
+        [HttpGet]
+        public IEnumerable<GradeInfoPerSubject> AvarageGradesPerSubjects()
+        {
+            return this.gradeLogic.GetAvarageGradesPerSubjects();
+        }
+        [HttpGet]
+        public int? NumbersOfStudentsThatHasFailed()
+        {
+            return this.gradeLogic.GetNumbersOfStudentsThatHasFailed();
+        }
+        [HttpGet]
+        public IEnumerable<ClassInfo> StudentsCountOfEachClass()
+        {
+            return this.studentLogic.GetStudentsCountOfEachClass();
+        }
+    }
+}
